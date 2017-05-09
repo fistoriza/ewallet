870616996002858e947c8ddab4606ea32b8ab92c
Fisto Riza Satianto <fistoriza@me.com>
2017-05-09 05:39:06 +0000

    validation

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -7,4 +7,19 @@ class WalletsController < ApplicationController
   def new
     @wallet = Wallet.new
   end
+
+  def create
+    @wallet = Wallet.create(wallet_params)
+    if @wallet.invalid?
+      flash[:error] = 'The data you entered is invalid'
+    end
+
+    redirect_to root_path
+  end
+
+  private
+
+  def wallet_params
+    params.require(:wallet).permit(:expense, :amount)
+  end
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,2 +1,4 @@
 class Wallet < ApplicationRecord
+  validates :expense, presence: true, length: { maximum: 50, minimum: 3 }
+  validates :amount, presence: true
 end
